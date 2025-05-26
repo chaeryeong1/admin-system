633db3159443067ad4270f3c1b89134baffbd824
chaeryeong1 <chaerom98@gmail.com>
2025-05-26 21:39:46 +0900

    fix: GitHub Pages 배포 설정 수정

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   root: 'admin-system-html',
+  base: '/admin-system/',
   build: {
     outDir: '../dist',
     emptyOutDir: true
